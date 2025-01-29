a3705fe059ebb6ac68694116fb4ec3835748c7c8
Антон <110550719+markovanton1986@users.noreply.github.com>
2025-01-29 22:08:26 +0600

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,25 +1,51 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import "./Home.css";
 
 const Home = () => {
+    const { user, isAuthenticated } = useSelector((state) => state.auth);
+    const userRole = user?.role;
+
     return (
         <div className="home-container">
-            <div style={{ textAlign: "center", padding: "20px" }}>
+            <div>
                 <h1>Добро пожаловать в облачное хранилище</h1>
-                <p>
-                    Это приложение позволяет безопасно хранить и управлять вашими файлами. 
-                    Для доступа к функционалу, пожалуйста, зарегистрируйтесь или войдите. 
-                    Надеюсь, Вы останетесь довольны при использовании данного приложения. Спасибо за внимание!
-                </p>
-                <div style={{ marginTop: "20px" }}>
-                    <Link to="/register" style={{ marginRight: "10px" }}>
-                        <button>Регистрация</button>
-                    </Link>
-                    <Link to="/login">
-                        <button>Войти</button>
-                    </Link>
-                </div>
+                {isAuthenticated ? (
+                    <>
+                        <p>
+                            Вы вошли в систему как {user.username}. Для дальнейшей работы в приложении, перейдите в свой раздел.
+                        </p>
+                        <div>
+                            {/* Условие отображения ссылок по роли */}
+                            {userRole === "admin" ? (
+                                <Link to="/admin">
+                                    <button>Управление пользователями</button>
+                                </Link>
+                            ) : (
+                                <Link to="/UserPage">
+                                    <button>Мои файлы</button>
+                                </Link>
+                            )}
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <p>
+                            Это приложение позволяет безопасно хранить и управлять вашими
+                            файлами. Для доступа к функционалу, пожалуйста, зарегистрируйтесь
+                            или войдите.
+                        </p>
+                        <div>
+                            <Link to="/register">
+                                <button>Регистрация</button>
+                            </Link>
+                            <Link to="/login">
+                                <button>Войти</button>
+                            </Link>
+                        </div>
+                    </>
+                )}
             </div>
         </div>
     );
