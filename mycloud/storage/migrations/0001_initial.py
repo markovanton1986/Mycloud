5867121e592ba271f6fa2703ae4576e540ee2308
Антон <110550719+markovanton1986@users.noreply.github.com>
2025-01-29 22:03:24 +0600

    Update 0001_initial.py

--- mycloud/storage/migrations/0001_initial.py
+++ mycloud/storage/migrations/0001_initial.py
@@ -30,6 +30,7 @@ class Migration(migrations.Migration):
                 ('last_name', models.CharField(blank=True, max_length=50)),
                 ('is_active', models.BooleanField(default=True)),
                 ('is_staff', models.BooleanField(default=False)),
+                ('role', models.CharField(choices=[('admin', 'Admin'), ('user', 'User')], default='user', max_length=50)),
                 ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
                 ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
             ],
